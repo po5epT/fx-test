1bfe894fdfae2070049c3568984d411efd67cefb
po5epT <megastorm87@mail.ru>
2023-09-02 12:18:49 +0300

    Creates a default layout

---
+++ src/layouts/default/DefaultLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="layout-default">
+        <slot name="header">
+            <TheHeader />
+        </slot>
+
+        <slot />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/layouts/default/TheHeader.vue';
+
+export default defineComponent({
+    name: 'DefaultLayout',
+
+    components: {
+        TheHeader,
+    },
+});
+</script>
---
+++ src/layouts/default/TheHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-3 px-md-4 mb-3 border-bottom">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
+    </div>
+</template>
